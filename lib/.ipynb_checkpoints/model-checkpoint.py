6ca11dcb036cbbaf6727f02fe52ee7511273a347
Ishaipiriyan Karunakularatnam <ikarunak@n23-64-512-pigwidgeon.hpc.hutton.ac.uk>
2024-10-10 14:06:33 +0100

    Added comments and print functions. Program now saves output in-place

--- lib/.ipynb_checkpoints/model-checkpoint.py
+++ lib/.ipynb_checkpoints/model-checkpoint.py
@@ -2,72 +2,119 @@ from PIL import Image
 import torch
 from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
 import lib.config as config
+import lib.utils as utils
 
+def load_model() -> object:
+    """
+    Load the Qwen2-VL-7B pretrained model, automatically setting to available device (GPU is given priority if it exists).
 
-def load_model():
+    Return:
+        model (object): Returns the loaded pretrained model.
+    """
     model = Qwen2VLForConditionalGeneration.from_pretrained(
-        "Qwen/Qwen2-VL-7B-Instruct", torch_dtype="auto", device_map="auto"
+        config.MODEL, torch_dtype="auto", device_map="auto"
     )
 
     return model
 
 
-def load_processor():
-    processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-7B-Instruct")
+def load_processor() -> object:
+    """
+    Loads the pre-processor that is used to pre-process the input prompt and images.
+
+    Return:
+        processor (object): Returns the loaded pretrained processor for the model.
+    """
+    processor = AutoProcessor.from_pretrained(config.MODEL)
 
     return processor
 
 
 def perform_inference(model: object, processor: object, images : list, max_new_tokens : int = None) -> list:
+    """
+    Performs inference on the given set of images.
 
-    #Preprocess the inputs
+    Parameters:
+        model (object): The pretrained model.
+        processor (object): The pretrained pre-processor.
+        images (list): A set of images to batch inference.
+        max_new_tokens (int): Maximum number of tokens (words) for the model to remember/learn.[Default: None]
+
+    Return:
+        output_text (list): A set of model outputs for given set of images.
+    """
+    # Preprocess the inputs
     max_new_tokens = max_new_tokens if (max_new_tokens is not None) else config.MAX_NEW_TOKENS
-    #Prepreocess the conversation
+    # Prepreocess the conversation
     text_prompt = processor.apply_chat_template(config.CONVERSATION, add_generation_prompt=True)
-    #Get N text_prompts for equal number of images
+    # Get N text_prompts for equal number of images
     text_prompts = [text_prompt] * len(images)
 
     images_opened = [Image.open(image) for image in images]
-    #Preprocess the inputs
+    # Preprocess the inputs
     inputs = processor(
         text=text_prompts, images=images_opened, padding=True, return_tensors="pt"
     )
-    #Move inputs to device (model automatically moves)
+    # Move inputs to device (model automatically moves)
     inputs = inputs.to("cuda" if torch.cuda.is_available() else "cpu") # 
 
-    #Inference
+    # Inference
     output_ids = model.generate(**inputs, max_new_tokens=max_new_tokens) 
-    ## Increasing the number of new tokens, increases the number of words recognised by the model with trade-off of speed
-    ## 1024 new tokens was capable of reading upto 70% of the input image (pg132_a.jpeg)
+    # Increasing the number of new tokens, increases the number of words recognised by the model with trade-off of speed
+    # 1024 new tokens was capable of reading upto 70% of the input image (pg132_a.jpeg)
     generated_ids = [
         output_ids[len(input_ids) :]
         for input_ids, output_ids in zip(inputs.input_ids, output_ids)
     ]
-    
+
+    # Using the preprocessor to decode the numerical values into tokens (words)
     output_text = processor.batch_decode(
         generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
     )
 
     return output_text
 
-def batch_infer(model: object, processor: object, images: list, batch_size: int = None, max_new_tokens: int = None) -> list:
+
+def batch_infer(model: object, processor: object, images: list, batch_size: int = None, max_new_tokens: int = None, save_path: str = None) -> list:
+    """
+    Perform batch inference on a list of images given a batch size
+
+    Parameters:
+        model (object): The pretrained model.
+        processor (object): The pretrained pre-processor.
+        images (list): A set of images to batch inference.
+        batch_size (int): The size of a single batch to inference. [Default: None]
+        max_new_tokens (int): Maximum number of tokens (words) for the model to remember/learn. [Default: None]
+
+    Return:
+        image_text_pairs (list): A list of tuples containing (image, output_text) pairs
+    """
+
+    # Define the value for maximum number of tokens and batch size given they are empty (None)
     max_new_tokens = max_new_tokens if (max_new_tokens is not None) else config.MAX_NEW_TOKENS
     batch_size = batch_size if (batch_size is not None) else config.BATCH_SIZE
+    print(f"
+    Using: \n Maximum new tokens = {max_new_tokens} \n
+    Batch size = {batch_size} \n
+    save_path = {save_path}
+    ")
 
-    
+    # Seperate the input images into batches (return the rest as a single batch)
+    # Such that given 10 images and a batch size of 3
+    # We get 3 batches of size 3 and a final batch of size 1
     batched_images = [images[x:min(x+batch_size, len(images))] for x in range(0, len(images), batch_size)]
 
-    image_text_pairs = []
-
-    for batch in batched_images:
-
+    # Loop through batched images and perform inference
+    for ind, batch in enumerate(batched_images):
+        print(f"Batch {ind+1} starting...")
         output_text = perform_inference(model, processor, batch, max_new_tokens)
-
+        
         pairs = [(image, output_text[ind]) for ind, image in enumerate(batch)]
 
-        image_text_pairs.extend(pairs)
-
-    return image_text_pairs
+        # Saving the output as soon as we get it for each batch, as not to waste memory
+        print(f"== Saving Pairs For Batch {ind+1} ==")
+        utils.save_jsons(pairs, save_path)
+    
         
 
     
--- lib/.ipynb_checkpoints/utils-checkpoint.py
+++ lib/.ipynb_checkpoints/utils-checkpoint.py
@@ -5,24 +5,42 @@ import lib.config as config
 
 
 def load_images(path: str) -> list:
+    """
+    Load (Unravel) images given a path to a directory or single image
 
+    If a nested directory is given, all images inside said nexted directories are also gathered.
+
+    Parameter:
+        path (str): the absolute path to a directory of images or an image
+
+    Return:
+        all_files (list): A list of all the possible images in a directory including any that are nested (includes the path to them)
+    """
+    
+    # Return just image if path is to an image (confirms by checking approved extensions)
     if os.path.isfile(path) and (path.split(".")[-1] in config.IMAGE_EXT):
         return [path]
     
     all_files = []
 
+    # Traverse through the directory
     for file in os.listdir(path):
+        # Get the file path
         file_path = os.path.join(path, file)
+
+        # Check if it is an image
         if not(os.path.isdir(file_path)):
             if (file.split(".")[-1] in config.IMAGE_EXT):
             
                 all_files.append(file_path)
         else:
+            # If directory then use recursion to find all images under it
             files = load_images(file_path)
             all_files.extend(files)
     
     return all_files
 
+    
 def clean_json(text: str) -> str:
     """
     Performs cleaning and normalisation of the input string (of JSON format from AI output)
@@ -33,15 +51,15 @@ def clean_json(text: str) -> str:
     Return:
         cleaned_text : a cleaned format of the initial text that is passable into JSON
     """
-    #Remove starting json word
+    # Remove starting json word
     cleaned_text = re.sub(r'^json', '', text)
-    #Remove any non-ASCII characters
+    # Remove any non-ASCII characters
     cleaned_text = re.sub(r'[^\x00-\x7F]+', '', cleaned_text)
-    #Make sure any delimeter or text are in double quotation such that string can be turned into a json file
+    # Make sure any delimeter or text are in double quotation such that string can be turned into a json file
     cleaned_text = re.sub(r'(?<=\w)"\s+(\[[^\]]+\])', r' \1"', cleaned_text)
-    #Remove duplicate quotation marks before : delimiter
+    # Remove duplicate quotation marks before : delimiter
     cleaned_text = re.sub(r'(?<=")":', r':', cleaned_text)
-    #Normalise any spaces or indentation
+    # Normalise any spaces or indentation
     cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
 
     return cleaned_text
@@ -66,6 +84,7 @@ def verify_json(text: str) -> bool:
 
     return False
 
+    
 def dump_json(text: str, file_name: str = "sample.json", save_path : str = None):
     """
     Dumps the input json-verified text into given file name at defined path
@@ -75,43 +94,77 @@ def dump_json(text: str, file_name: str = "sample.json", save_path : str = None)
         file_name (str) : file name to dump the json-text in
         save_path (str) : path to save directory (defined in settings if None)
     """
-    #Find the save directory in config if not provided
+    # Find the save directory in config if not provided
     save_path = save_path if (save_path is not None) else config.SAVE_PATH
 
-    #Load the json text into json
+    # Load the json text into json
     json_loaded_text = json.loads(text)
 
-    #Save the file
+    # Save the file
     with open(os.path.join(save_path, file_name), "w") as json_file:
         json.dump(json_loaded_text, json_file, indent=4)
 
+
 def save_text(text: str, file_name: str = "sample.txt", save_path : str = None):
-    #Find the save directory in config if not provided
+    """
+    Save the given text in a text document for debugging purposes
+
+    Parameters:
+        text (str) : text to dump into txt file
+        file_name (str) : file name to dump the text in
+        save_path (str) : path to save directory (defined in settings if None)
+    """
+    # Find the save directory in config if not provided
     save_path = save_path if (save_path is not None) else config.SAVE_PATH
     
-    #Save the file
-    with open(os.path.join(save_path, file_name), "w") as file_:
-        file_.write(text)
-        
-def save_json(image: str, text: str, save_path: str = None):
+    # Save the file
+    with open(os.path.join(save_path, file_name), "w") as txt_file:
+        txt_file.write(text)
 
+
+def save_json(image: str, text: str, save_path: str = None):
+    """
+    Perform cleaning and save the input text into a JSON format (if possible) else a Text Document
+
+    Parameters:
+        image (str): the path to the image
+        text (str): the output text from the model
+        save_path (str): the path to save the model in
+    """
+    print(f">>> Saving text output for {image}")
+    # Cleaning the text here
     json_text = text.split("```")[1]
     json_text = clean_json(json_text)
 
+    # defining the save path if it does not exist
     save_path = save_path if (save_path is not None) else os.path.join(os.sep.join(image.split(os.sep)[:-1]), "text_files")
 
+    # Makes sure th save path exists
     if not(os.path.exists(save_path)):
         os.mkdir(save_path)
-        
+
+    # Get the file name of the image
     file_name = image.split(os.sep)[-1].split(".")[0] 
-    
+
+    # verify if text is json formatted
     if verify_json(json_text):
+        # Dump into json file
         dump_json(json_text, file_name+ ".json", save_path)
+    else:
+        # If not, save it as a text file
+        save_text(json_text, file_name+ ".txt", save_path)
 
-    save_text(json_text, file_name+ ".txt", save_path)
+
 def save_jsons(image_json_pairs: list, save_path: str = None):
+    """
+    Iterate thrrough all image-text pairs and save them
 
+    Parameters:
+        image_json_pairs (list): image-json pairs to save
+        save_path (str): the path in which to save
+    """
     for image, text in image_json_pairs:
+        
         save_json(image, text, save_path)
     
 
