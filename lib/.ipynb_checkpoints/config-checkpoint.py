c0175eb8309600e8ee867780d2e17dccbad39c5d
Ishaipiriyan Karunakularatnam <ikarunak@n23-64-512-pigwidgeon.hpc.hutton.ac.uk>
2024-10-24 16:04:01 +0100

    Simplified and optimised qwen model deployment

--- lib/.ipynb_checkpoints/config-checkpoint.py
+++ lib/.ipynb_checkpoints/config-checkpoint.py
@@ -8,11 +8,24 @@ IMAGE_EXT = ["jpeg", "png", "jpg"]
 IGNORE_FILE = [".ipynb_checkpoints"]
 # Path to the save directory of the json files
 SAVE_PATH = "./parsed_json"
+CROPPED_DIR_NAME = "cropped"
 
-PROMPT = "Process this image in two steps: Step 1: Turn the text in this picture into markdown. Indicate italics. \
-            Indent lines which discuss folder contents as unordered lists Step 2: Convert the markdown text that you created in step 1 into JSON. \
-            Use the heading texts as keys, and the folder details as nested numbered lists\
-            Your output should consist of the markdown text, then a fenced JSON code block"
+PROMPT = """The image consists of a catalogue of plant collection names from the Lightfoot collection. It follows the hierarchy of Taxon name, species name and the collection inside the folder consisting of species name, where it was collected, who collected it (citation) and other meta data.
+
+I want you to extract the information from the catalogue and prepare a structured json output. If a text does not have taxon name or species name either at the start or at the very end, please mark that down as extra_COUNTER.
+
+Example:
+
+{"CAPRIFOLIACEAE" : {"Linnaea borealis L.": [ {"folder": 1, "content": "Linnea borealis [JL]. i. Cites Linn. Sp. Pl. 631; Bauh. Pin. 93"} ] }
+}
+
+Example (extra data at the start or end of image without taxon):
+
+{"extra_data_1":{"content": "Folder 2. Campanula hederacea [G]. i. "Devon & Cornwal" [JL]"},
+"CAPRIFOLIACEAE" : {"Linnaea borealis L.": [ {"folder": 1, "content": "Linnea borealis [JL]. i. Cites Linn. Sp. Pl. 631; Bauh. Pin. 93"} ] }
+}
+
+Your output should be a structured json format as shown above in the examples and not the example itself."""
 
 DEFAULT_PROMPT = "Process this image in two steps: Step 1: Turn the text in this picture into markdown. Indicate italics. \
             Indent lines which discuss folder contents as unordered lists Step 2: Convert the markdown text that you created in step 1 into JSON. \
--- lib/.ipynb_checkpoints/model-checkpoint.py
+++ lib/.ipynb_checkpoints/model-checkpoint.py
@@ -4,117 +4,154 @@ from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoPro
 import lib.config as config
 import lib.utils as utils
 
-def load_model() -> object:
-    """
-    Load the Qwen2-VL-7B pretrained model, automatically setting to available device (GPU is given priority if it exists).
 
-    Return:
-        model (object): Returns the loaded pretrained model.
-    """
-    model = Qwen2VLForConditionalGeneration.from_pretrained(
-        config.MODEL, torch_dtype="auto", device_map="auto"
-    )
+class QWEN_Model:
 
-    return model
+    MODEL_NAME = config.MODEL
 
+    def __init__(self, 
+                 prompt: str = None, # Input prompt into the model
+                 conversation :list = None, # Input conversation into the model
+                 batch_size: int = 3, # Batch size for inference
+                 max_new_tokens: int = 5000, # Maximum number of tokens
+                 save_path: str = None # Where to save the outputs
+                ):
 
-def load_processor() -> object:
-    """
-    Loads the pre-processor that is used to pre-process the input prompt and images.
+        # Load parameters
+        self.prompt = prompt
+        self.conversation = self.getConversation(conversation)
+        self.batch_size = batch_size if (batch_size is not None) else config.BATCH_SIZE
+        self.max_new_tokens = max_new_tokens if (max_new_tokens is not None) else config.MAX_NEW_TOKENS
+        self.save_path = save_path
 
-    Return:
-        processor (object): Returns the loaded pretrained processor for the model.
-    """
-    processor = AutoProcessor.from_pretrained(config.MODEL)
+        # Load model
+        self.model = self.load_model()
+        # Load processor
+        self.processor = self.load_processor()
 
-    return processor
+    def getConversation(self, conversation: list) -> list:
 
-
-def perform_inference(model: object, processor: object, images : list, max_new_tokens : int = None, conversation :list = None) -> list:
-    """
-    Performs inference on the given set of images.
-
-    Parameters:
-        model (object): The pretrained model.
-        processor (object): The pretrained pre-processor.
-        images (list): A set of images to batch inference.
-        max_new_tokens (int): Maximum number of tokens (words) for the model to remember/learn.[Default: None]
-
-    Return:
-        output_text (list): A set of model outputs for given set of images.
-    """
-    # Preprocess the inputs
-    max_new_tokens = max_new_tokens if (max_new_tokens is not None) else config.MAX_NEW_TOKENS
-    # Prepreocess the conversation
-    conversation = conversation if conversation is not None else config.CONVERSATION
-    text_prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
-    # Get N text_prompts for equal number of images
-    text_prompts = [text_prompt] * len(images)
-
-    images_opened = [Image.open(image) for image in images]
-    # Preprocess the inputs
-    inputs = processor(
-        text=text_prompts, images=images_opened, padding=True, return_tensors="pt"
-    )
-    # Move inputs to device (model automatically moves)
-    inputs = inputs.to("cuda" if torch.cuda.is_available() else "cpu") # 
-
-    # Inference
-    output_ids = model.generate(**inputs, max_new_tokens=max_new_tokens) 
-    # Increasing the number of new tokens, increases the number of words recognised by the model with trade-off of speed
-    # 1024 new tokens was capable of reading upto 70% of the input image (pg132_a.jpeg)
-    generated_ids = [
-        output_ids[len(input_ids) :]
-        for input_ids, output_ids in zip(inputs.input_ids, output_ids)
-    ]
-
-    # Using the preprocessor to decode the numerical values into tokens (words)
-    output_text = processor.batch_decode(
-        generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
-    )
-
-    return output_text
-
-
-def batch_infer(model: object, processor: object, images: list, batch_size: int = None, max_new_tokens: int = None, save_path: str = None) -> list:
-    """
-    Perform batch inference on a list of images given a batch size
-
-    Parameters:
-        model (object): The pretrained model.
-        processor (object): The pretrained pre-processor.
-        images (list): A set of images to batch inference.
-        batch_size (int): The size of a single batch to inference. [Default: None]
-        max_new_tokens (int): Maximum number of tokens (words) for the model to remember/learn. [Default: None]
-
-    Return:
-        image_text_pairs (list): A list of tuples containing (image, output_text) pairs
-    """
-
-    # Define the value for maximum number of tokens and batch size given they are empty (None)
-    max_new_tokens = max_new_tokens if (max_new_tokens is not None) else config.MAX_NEW_TOKENS
-    batch_size = batch_size if (batch_size is not None) else config.BATCH_SIZE
-    print(f">>> Using: \n \tMaximum new tokens = {max_new_tokens} \n \tBatch size = {batch_size} \n \tsave_path = {save_path}")
-
-    # Seperate the input images into batches (return the rest as a single batch)
-    # Such that given 10 images and a batch size of 3
-    # We get 3 batches of size 3 and a final batch of size 1
-    batched_images = [images[x:min(x+batch_size, len(images))] for x in range(0, len(images), batch_size)]
-
-    # Loop through batched images and perform inference
-    for ind, batch in enumerate(batched_images):
-        print(f">>> Batch {ind+1} starting...")
-        output_text = perform_inference(model, processor, batch, max_new_tokens)
+        if self.prompt is None and conversation is None:
+            return config.CONVERSATION
+        elif self.prompt is not None and conversation is None:
+            return config.get_conversation(self.prompt)
+        else:
+            return conversation
+            
         
-        pairs = [(image, output_text[ind]) for ind, image in enumerate(batch)]
-
-        # Saving the output as soon as we get it for each batch, as not to waste memory
-        print(f"\t== Saving Pairs For Batch {ind+1} ==")
-        utils.save_jsons(pairs, save_path)
-        print(f"\t== Saving Done For Batch {ind+1}  ==")
+    def load_model(self) -> object:
+        """
+        Load the Qwen2-VL-7B pretrained model, automatically setting to available device (GPU is given priority if it exists).
     
+        Return:
+            model (object): Returns the loaded pretrained model.
+        """
+        model = Qwen2VLForConditionalGeneration.from_pretrained(
+            self.MODEL_NAME, torch_dtype="auto", device_map="auto"
+        )
+    
+        return model
+
+
+    def load_processor(self) -> object:
+        """
+        Loads the pre-processor that is used to pre-process the input prompt and images.
+    
+        Return:
+            processor (object): Returns the loaded pretrained processor for the model.
+        """
+        processor = AutoProcessor.from_pretrained(self.MODEL_NAME)
+    
+        return processor
+
+    def __call__(self, images: list, debug: bool=False) -> list:
+        """
+        Performs inference on the given set of images.
+    
+        Parameters:
+            images (list): A set of images to batch inference.
+    
+        Return:
+            output_text (list): A set of model outputs for given set of images.
+        """
+
+        # Process the input conversation
+        utils.debugPrint("\tProcessing text prompts...", debug)
+        text_prompt = self.processor.apply_chat_template(self.conversation, add_generation_prompt=True)
+        # Get N text_prompts for equal number of images
+        text_prompts = [text_prompt] * len(images)
+
+        utils.debugPrint("\tReading Images...", debug)
+        # Open the images from the paths
+        images_opened = [Image.open(image) for image in images]
+        # Preprocess the inputs
+        utils.debugPrint("\tProcessing inputs...", debug)
+        inputs = self.processor(
+            text=text_prompts, images=images_opened, padding=True, return_tensors="pt"
+        )
+
+        utils.debugPrint("\tMoving inputs to gpu...", debug)
+        # Move inputs to device (model automatically moves)
+        inputs = inputs.to("cuda" if torch.cuda.is_available() else "cpu") # 
+    
+        # Inference
+        utils.debugPrint("\tPerforming inference...", debug)
+        output_ids = self.model.generate(**inputs, max_new_tokens=self.max_new_tokens) 
+        # Increasing the number of new tokens, increases the number of words recognised by the model with trade-off of speed
+        # 1024 new tokens was capable of reading upto 70% of the input image (pg132_a.jpeg)
+        utils.debugPrint("\tInference Finished", debug)
+        output_ids = output_ids.cpu()
+
+        utils.debugPrint("\tSeperating Ids...", debug)
+        generated_ids = [
+            output_ids[len(input_ids) :]
+            for input_ids, output_ids in zip(inputs.input_ids, output_ids)
+        ]
+
+        # Using the preprocessor to decode the numerical values into tokens (words)
+        utils.debugPrint("\tDecoding Ids...", debug)
+        output_text = self.processor.batch_decode(
+            generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
+        )
+
+
+        return output_text
+
+    def batch_infer(self, images: list, save: bool = True, debug: bool=False) -> list:
+        """
+        Perform batch inference on a list of images given a batch size
+    
+        Parameters:
+            images (list): A set of images to batch inference.
+    
+        Return:
+            image_text_pairs (list): A list of tuples containing (image, output_text) pairs
+        """
+    
+        # Define the value for maximum number of tokens and batch size given they are empty (None)
+        print(f">>> Using: \n \tMaximum new tokens = {self.max_new_tokens} \n \tBatch size = {self.batch_size} \n \tsave_path = {self.save_path}")
+    
+        # Seperate the input images into batches (return the rest as a single batch)
+        # Such that given 10 images and a batch size of 3
+        # We get 3 batches of size 3 and a final batch of size 1
+        batched_images = [images[x:min(x+self.batch_size, len(images))] for x in range(0, len(images), self.batch_size)]
+
+        all_pairs = []
+        # Loop through batched images and perform inference
+        for ind, batch in enumerate(batched_images):
+            print(f">>> Batch {ind+1} starting...")
+            output_text = self(batch, debug)
+            utils.debugPrint("\tSeperating Outputs...", debug)
+            pairs = [(image, output_text[ind]) for ind, image in enumerate(batch)]
+            all_pairs.extend(pairs)
+            utils.debugPrint("\tOutputs stored!", debug)
+            # Saving the output as soon as we get it for each batch, as not to waste memory or to repeat due to interruptions
+            if save:
+                print(f"\t== Saving Pairs For Batch {ind+1} ==")
+                utils.save_jsons(pairs, save_path)
+                print(f"\t== Saving Done For Batch {ind+1}  ==")
         
 
-    
+        return all_pairs
 
     
