c90b2b6a419ef9b5c2d0cdcede14b704a638ba75
ipiriyan2002 <ipiriyan2002@gmail.com>
2025-03-19 16:06:53 +0000

    Updated run.py to handle no output folder

--- run.py
+++ run.py
@@ -47,6 +47,9 @@ def main():
     args = parse_args()
     logger.info(f"Input arguments: {args}")
 
+    if not(os.path.exists(args.save_path)):
+        os.makedirs(args.save_path)
+
     # Load model
     logger.info(">>> Loading Model...")
 
